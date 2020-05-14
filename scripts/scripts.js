8c9c3a44e8e520fdf83170c7d66d589c553efa3e
Tom <holt.thomas@gmail.com>
2020-05-14 17:56:58 +0100

    Updates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,14 +5,36 @@ const nav = document.querySelector('.nav');
 const heroSection = document.querySelector('.hero');
 const header = document.querySelector('.page-header');
 const homeSectionText = document.querySelectorAll('.home-section-text');
+const heroVideo = document.querySelector('.hero video');
+const showreelButton = document.querySelector('.showreel-play-button');
+const modal = document.querySelector('.modal-outer');
+const modalCloseButton = document.querySelector('.close-modal-button');
+let showreelOpened = false; 
+
+
+
+
+
 
 
 
 // TODO prevent focus on nav when closed and on body when open 
 
+function checkIfVideoLoaded() {
+    if (heroVideo.src) {
+        console.log('video loaded');
+
+    } else {
+        setTimeout(checkIfVideoLoaded, 250);
+    }
+}
+
+checkIfVideoLoaded();
+
 function openNav() {
     nav.classList.add('nav-open');
-    body.classList.add('nav-open');
+    body.classList.add('no-scroll');
+    heroVideo.pause();
     setTimeout(() => {
         menuOpenButton.style.opacity = 0;
     }, 200);
@@ -20,16 +42,55 @@ function openNav() {
 
 function closeNav() {
     nav.classList.remove('nav-open');
-    body.classList.remove('nav-open');
+    body.classList.remove('no-scroll');
+    heroVideo.play();
     setTimeout(() => {
         menuOpenButton.style.opacity = 1
     }, 200);
 }
 
+function openModal() {
+    modal.classList.add('modal-open');
+    heroVideo.pause();
+    if (!showreelOpened) {
+    // https://github.com/sampotts/plyr/#options
+    const player = new Plyr('#player', {
+        autoplay: true,
+    });
+    // Expose player so it can be used from the console
+    window.player = player;
+
+    showreelOpened = true;
+    } else {
+        player.play();
+    }
+
+}
+
+function closeModal() {
+    modal.classList.remove('modal-open');
+    player.pause();
+    heroVideo.play();
+}
+
+showreelButton.addEventListener('click', openModal);
+modalCloseButton.addEventListener('click', closeModal);
 menuOpenButton.addEventListener('click', openNav);
 menuCloseButton.addEventListener('click', closeNav);
+modal.addEventListener('click', (e) => {
+    if (e.target === e.currentTarget) {
+        closeModal();
+    }  
+});
+window.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+      closeModal();
+      closeNav();
+    }
+  })
 
 
+// Change colour of header on scrol
 const headerScrollOptions = {
     rootMargin: "-90% 0px 0px 0px"
 };
@@ -64,6 +125,20 @@ const homeTextObserver = new IntersectionObserver(homeTextCallback, {
 });
 
 homeSectionText.forEach((text) => homeTextObserver.observe(text));
+
+// Showreel player
+
+// Change "{}" to your options:
+// https://github.com/sampotts/plyr/#options
+const player = new Plyr('#player', {
+    autoplay: true,
+});
+
+// Expose player so it can be used from the console
+window.player = player;
+
+
+
 
 
 // Set the year at the bottom of the page
